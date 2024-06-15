b61bced1b41483437f1dea46f600ecb8071e3d1e
Nanditha <nanditha.arun92@gmail.com>
2024-06-15 15:29:53 -0400

    base-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-app';
+  iconRotation = 90;
+  color: string = '#5B0888';
+  colors = [
+    {
+      css: '#FDE767',
+      name: 'Yellow'
+    },
+    {
+      css: '#FE7A36',
+      name: 'Orange'
+    },
+    {
+      css: '#910A67',
+      name: 'Magenta'
+    },
+    {
+      css: '#D63484',
+      name: 'Pink'
+    },
+    {
+      css: '#5B0888',
+      name: 'Default'
+    }
+
+  ]
+  rotateIconRight() {
+    this.iconRotation = this.iconRotation + 90;
+    this.iconRotation = this.iconRotation > 360 ? this.iconRotation - 360 : this.iconRotation;
+  }
+  rotateIconLeft() {
+    console.log('rotateIconLeft()');
+    this.iconRotation = this.iconRotation - 90;
+    this.iconRotation = this.iconRotation < 360 ? this.iconRotation + 360 : this.iconRotation;
+  }
+
+  changeColor(newColor) {
+    this.color = newColor;
+  }
+
 }
+
